b002b85073fe37dc7fdee29b6e016d319bd6eb00
Ras-AlGhoul <yazann.abbas@gmail.com>
2021-11-29 16:00:29 +0100

    fix links

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "18154a9df3a9e433"; }
+/******/ 		__webpack_require__.h = function() { return "2feb0d0ef7295831"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
