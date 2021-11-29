840562e2b0dafae9159068f71b855db5751b2767
Ras-AlGhoul <yazann.abbas@gmail.com>
2021-11-29 16:32:16 +0100

    fix merge

--- client/pages/labs.js
+++ client/pages/labs.js
@@ -1,64 +1,52 @@
 import { useState, useEffect } from "react"
 import NavPrivate from "../components/NavPrivate";
-import Videos from "../components/Videos"
-<<<<<<< HEAD
-import Github from "../components/Github"
+import Videos from "../components/Videos";
+import Github from "../components/Github";
+import Slides from "../components/Slides";
 
 const labs = () => {
+  const [slides, setSlides] = useState([]);
   const [videos, setVideos] = useState([]);
   const [githubLinks, setGithub] = useState([]);
-=======
-import Slides from "../components/Slides"
 
-const labs = () => {
-  const [videos, setVideos] = useState([]);
-  const [slides, setSlides] = useState([]);
->>>>>>> main
 
   const fetchVideos = async () =>{
    const res = await fetch('http://localhost:4000/api/videos')
    const allVideos = await res.json();
    setVideos(allVideos)
    return allVideos;
-  }
-<<<<<<< HEAD
+  };
+
   const fetchGithub = async () =>{
     const res = await fetch('http://localhost:4000/api/github')
     const allGithub = await res.json();
     setGithub(allGithub)
     return allGithub;
-   }
-  useEffect(() => {
-    fetchVideos();
-    fetchGithub();
-=======
-  const fetchSlides = async () =>{
-    const res = await fetch('http://localhost:4000/api/slides')
-    const allSlides = await res.json();
-    setSlides(allSlides)
-    return allSlides;
-   }
+   };
+
+   const fetchSlides = async () =>{
+     const res = await fetch('http://localhost:4000/api/slides')
+     const allSlides = await res.json();
+     setSlides(allSlides)
+     return allSlides;
+    };
+  
   useEffect(() => {
     fetchVideos();
     fetchSlides();
->>>>>>> main
+    fetchGithub();
     return () => {
     }
-  }, [])
+  }, []);
+
   return (
     <div>
-<<<<<<< HEAD
-    
-      labs Page
+      <NavPrivate />
       {videos.map(video => <Videos video={video} />)}
       {githubLinks.map(github => <Github github={github} />)}
-=======
-      <NavPrivate />
       {slides.map(slide => <Slides slide={slide} />)}
-      {videos.map((video, index) => <Videos video={video} key={index}/>)}
->>>>>>> main
     </div>
   )
 }
 
-export default labs
+export default labs;
