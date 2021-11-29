f468a5389fc18d35f31a97e18cbf14b5d4e9f2ab
Ras-AlGhoul <yazann.abbas@gmail.com>
2021-11-29 15:59:26 +0100

    fix img links

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d4e131ebc1bc883a"; }
+/******/ 		__webpack_require__.h = function() { return "18154a9df3a9e433"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/components/Slides.js
+++ client/components/Slides.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Slides = ({slide}) => {
   return (
     <div>
-      <a target='_blank' href={slide.link}><img src='https://i.ibb.co/pfjKf76/Google-Slides-logo-2014-2020-svg.png'></img></a>
+      <a target='_blank' href={slide.link}><img src='https://i.postimg.cc/VN6C7t0S/4.png'></img></a>
     </div>
   )
 }
