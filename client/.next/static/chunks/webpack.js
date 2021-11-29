012cc74d0cb15c2c69315f61cc79502797335363
Ras-AlGhoul <yazann.abbas@gmail.com>
2021-11-29 16:16:59 +0100

    main

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2feb0d0ef7295831"; }
+/******/ 		__webpack_require__.h = function() { return "71f4576dfa35f760"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
