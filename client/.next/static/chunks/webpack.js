60b3b12bc5e7062ab7f845a3c328f6459e9df26d
Ras-AlGhoul <yazann.abbas@gmail.com>
2021-11-30 13:09:22 +0100

    next server

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2bf0dd970490f453"; }
+/******/ 		__webpack_require__.h = function() { return "6d9acb4c2724ac30"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
