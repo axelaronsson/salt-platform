dd3b15742d294512e22bc5848e196b4a672f5920
AndelijaKaluderovic <andeka011@gmail.com>
2021-12-01 09:43:53 +0100

    basic setup

---
+++ components/Creators.js
@@ -0,0 +1,14 @@
+const Creators = () => {
+    return (
+        <div>
+           <p>We are Chillies....</p>
+           <div>
+               <div><img src="https://i.postimg.cc/pVFphzYf/Yazan.png"></img><a href="https://github.com/Ras-AlGhoul">Yazan</a></div>
+               <div><img src="https://i.postimg.cc/rwmTWLXB/Angie.png"></img><a href="https://github.com/AndelijaKaluderovic">Angie</a></div>
+               <div><img src="https://i.postimg.cc/26pNPbbY/Axel.png"></img><a href="https://github.com/axelaronsson">Axel</a></div>
+           </div>
+        </div>
+    )
+}
+
+export default Creators
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,29 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Creators from '../components/Creators'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h2>Salt Platform</h2>
+      <button>Login</button>
+      <div>
+        <h3>About Salt </h3>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <div>
+        About Salt Platform
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+      <div>
+        <h3>Creators</h3>
+      <Creators />
+      </div>
     </div>
   )
 }
