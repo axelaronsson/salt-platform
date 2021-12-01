a1a8c295b7785fac766e833d0b6628f172666a88
Ras-AlGhoul <yazann.abbas@gmail.com>
2021-12-01 21:39:22 +0100

    fix bug

--- pages/createAccount.js
+++ pages/createAccount.js
@@ -1,27 +1,42 @@
+import { useState } from 'react';
 import styles from '../styles/createAccount.module.css'
+import axios from 'axios';
 
 const createAccount = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(email, password);
+    const newAccount = {
+      email,
+      password
+    }
+    setEmail('');
+    setPassword('');
+  }
   return (
     <div className={styles.container}>
-    
-    <div className={styles.imgdiv}>
-    <img src='https://i.postimg.cc/QxWPMByG/signup-image.jpg'/>
-    </div>
 
-    <div className={styles.formdiv}>
-    <h1>&lt;/Salt&gt;</h1>
-    <form>
-      <label><strong>Email</strong></label><br/>
-      <input className={styles.input}/>
-      <hr/>
-      <label><strong>Password</strong></label><br/>
-      <input className={styles.input}/>
-      <hr/>
-      <button className={styles.button} type='submit'>Create Account</button>
-    </form>
-    </div>
+      <div className={styles.imgdiv}>
+        <img src='https://i.postimg.cc/QxWPMByG/signup-image.jpg' />
+      </div>
 
-  </div>
+      <div className={styles.formdiv}>
+        <h1>&lt;/Salt&gt;</h1>
+        <form onSubmit={handleSubmit}>
+          <label><strong>Email</strong></label><br />
+          <input value={email} onChange={({ target: { value } }) => setEmail(value)} className={styles.input} />
+          <hr />
+          <label><strong>Password</strong></label><br />
+          <input type='password' value={password} onChange={({ target: { value } }) => setPassword(value)} className={styles.input} />
+          <hr />
+          <button className={styles.button} type='submit'>Create Account</button>
+        </form>
+      </div>
+
+    </div>
   )
 };
 
