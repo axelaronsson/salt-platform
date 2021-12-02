c9b2dbc0f11f9dc7066bc1e557922b9cfdd332b6
Ras-AlGhoul <yazann.abbas@gmail.com>
2021-12-02 11:27:51 +0100

    USER SCHEMA

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new Schema({
 	name: {
@@ -10,6 +11,7 @@ const UserSchema = new Schema({
 	},
 	email: {
 		type: String,
+		unique: true,
 		required: true,
 		lowercase: true,
 		validate(value) {
@@ -30,15 +32,48 @@ const UserSchema = new Schema({
 				throw new Error('password is not a valid password')
 			}
 		},
+	},
+	mobile_number: {
+		type: String,
+		validate(value) {
+				if (!validator.isMobilePhone(value)) {
+						throw new Error('user must provide a valid phone number');
+				}
+		}
+},
+admission_date: {
+	type: String,
+	validate(value) {
+			if (!validator.isDate(value)) {
+					throw new Error('user must provide a valid date. e.g. [2021-07-15]');
+			}
 	}
-}
-);
+
+},
+});
+
+UserSchema.statics.findByCredentials = async (email, password) => {
+	const user = await User.findOne({ email });
+	if(!user){
+		throw new Error('unable to login')
+	}
+	const isMatch = await bcrypt.compare(password, user.password)
+	if(!isMatch){
+		throw new Error('wrong password')
+	}
+	return user;
+};
 
 UserSchema.pre('save',async function(next){
   const user = this;
+	console.log(validator.isMobilePhone('ss'));
 	console.log('just before saving');
+	if(user.isModified('password')){
+		user.password = await bcrypt.hash(user.password, 8)
+	}
 	next();
-})
+});
+
 const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,37 +2,57 @@ const express = require('express');
 const userRouter = express.Router();
 const { reqBodyValidator, idValidator, nextId } = require('../errorHandling');
 const User = require('../db/models/User');
-const bcrypt = require('bcryptjs');
 require('../db/mogoose');
 
 userRouter
   .route('/')
   .get((req, res) => User.find({}).then(response => res.send(response)))
-  .post( async (req, res) => {
+  .post(async (req, res) => {
     res.setHeader('content-type', 'application/json');
     reqBodyValidator(req);
     const user = new User(req.body);
     user.save().then(response => res.send(response));
   });
 
+userRouter.post('/login', async(req, res) => {
+  console.log('im here');
+  console.log(req.body)
+  try{
+    const user = await User.findByCredentials(req.body.email, req.body.password);
+    res.send(user);
+  }catch(e){
+    res.status(400).send(e.message)
+  }
+})
+
 userRouter
   .route('/:id')
-  .put((req, res) => {
-    res.setHeader('content-type', 'application/json');
-    const { id } = req.params;
-    idValidator(id, items);
-    const editedItem = {
-      id: id,
-      link: req.body.link,
+  .put(async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'mobile_number', 'admission_date']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+      return res.status(400).send({ error: 'Invalid updates!' })
     }
-    items = items.map(item => item.id === id ? editedItem : item);
-    res.status(204);
-    res.end();
+
+    try {
+      const user = await User.findById(req.params.id);
+      updates.forEach(update => user[update] = req.body[update]);
+      await user.save();
+      if (!user) {
+        return res.status(404).send()
+      }
+
+      res.send(user)
+    } catch (e) {
+      res.status(400).send(e)
+    }
   })
-  .delete( async (req, res) => {
+  .delete(async (req, res) => {
     const { id } = req.params;
     // idValidator(id, items);
-    await User.deleteOne({_id: id});
+    await User.deleteOne({ _id: id });
     console.log(id);
     res.status(204);
     res.end()
